77381f7bc6c6225bf03db0871e378e33f7f2e82a
Seth McLaughlin <s@sethmcl.com>
2012-06-25 15:47:24 -0700

    Updating executor and adding some tests

--- test/executor.js
+++ test/executor.js
@@ -1,8 +1,20 @@
 /**
  * @author LinkedIn
  */
-var should   = require('chai').should(),
-    executor = require('../lib/executor');
+var sinonChai = require('./lib/sinon-chai'),
+    sinon     = require('sinon'),
+    executor  = require('../lib/executor'),
+    io        = require('socket.io');
 
 describe('lib/executor', function() {
+  it('should connect to socket-io server on instantiation', function(done) {
+    var fakeMasterServer = io.listen(3333);
+    fakeMasterServer.disable('log');
+
+    fakeMasterServer.on('connection', function(socket) {
+      done();
+    });
+
+    new executor.Executor({ masterUrl: 'http://localhost:3333' });
+  });
 });
