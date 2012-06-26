81ecf4ef37d5eb2b4743f607c3a49ac0486ac247
Seth McLaughlin <s@sethmcl.com>
2012-06-25 21:10:49 -0700

    Switched templating engine from ejs to dust

--- locales/pirate.js
+++ locales/pirate.js
@@ -2,5 +2,8 @@
 	"Starting in master mode": "Arrrrr mateys! Starting in master mode",
 	"Starting in executor mode": "Starting in executor mode",
 	"Master Server is running on %s:%s": "Ahoy! Ye Master Server be runnin' on %s:%s",
-	"Capture page, running at %s:%s": "Capture page, running at %s:%s"
+	"Capture page, running at %s:%s": "Capture page, running at %s:%s",
+	"Capture page running on %s:%s": "Capture page running on %s:%s",
+	"Master running on %s:%s": "Master running on %s:%s",
+	"Capture this browser": "Capture this browser"
 }
