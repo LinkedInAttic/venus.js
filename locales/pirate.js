43de2b3c2a840f99f52d693352134c12eeffdb3c
Seth McLaughlin <s@sethmcl.com>
2012-06-25 20:30:42 -0700

    Added i18n support to master views

--- locales/pirate.js
+++ locales/pirate.js
@@ -1,4 +1,6 @@
 {
 	"Starting in master mode": "Arrrrr mateys! Starting in master mode",
-	"Starting in executor mode": "Starting in executor mode"
+	"Starting in executor mode": "Starting in executor mode",
+	"Master Server is running on %s:%s": "Ahoy! Ye Master Server be runnin' on %s:%s",
+	"Capture page, running at %s:%s": "Capture page, running at %s:%s"
 }
