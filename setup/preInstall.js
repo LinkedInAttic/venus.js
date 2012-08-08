985b4f59ac1826658edaba2ecacdad934ce14095
Ali Razfar <ali.razfar@gmail.com>
2012-08-08 15:32:17 -0700

    fixed preInstall script to accomodate phantomjs-node and git submodule

--- setup/preInstall.js
+++ setup/preInstall.js
@@ -8,6 +8,9 @@ if (process.platform === 'darwin') {
   unpack('tar -xvf phantomjs-1.6.0-linux-x86_64-dynamic.tar.tar.bz2 && mv phantomjs-1.6.0-linux-x86_64-dynamic phantomjs-1.6.0');
 }
 
+submoduleCommand();
+phantomjsNPMinstall();
+
 // remove the old phantomjs directory
 // and unpack the proper file
 function unpack(unzipCommand) {
@@ -17,6 +20,24 @@ function unpack(unzipCommand) {
     unzipCommand,
     'cd phantomjs-1.6.0/bin',
     'mv phantomjs phantomjs-venus'
+  ].join(' && ');
+
+  exec(command);
+}
+
+function submoduleCommand() {
+  var command = [
+    'git submodule init',
+    'git submodule update'
+  ].join(' && ');
+
+  exec(command);
+}
+
+function phantomjsNPMinstall() {
+  var command = [
+    'cd phantomjs-node',
+    'npm install'
   ].join(' && ');
 
   exec(command);
