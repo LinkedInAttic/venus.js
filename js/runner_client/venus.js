4344e8be0adcd061d94be0093a193abff0fbcc9c
Asa Kusuma <akusuma@linkedin.com>
2012-09-27 14:07:26 -0700

    Fix issue in Firefox 3.6

--- js/runner_client/venus.js
+++ js/runner_client/venus.js
@@ -4,7 +4,6 @@
   function Venus() {}
 
   Venus.prototype.done = function(results) {
-  	console.log(results);
     $.post(window.venus.postUrl, JSON.stringify(results));
     console.log('posted results to', window.venus.postUrl);
   };
