835c9c365ee4870da0917a9411582e6c4823548a
Roel Ramirez <rramirez@rramirez-ld.linkedin.biz>
2012-09-26 17:34:26 -0700

    mocha adapter can now handle multiple describe blocks

--- .venus/adaptors/mocha-venus.js
+++ .venus/adaptors/mocha-venus.js
@@ -18,26 +18,38 @@ Adaptor.prototype.start = function() {
   mocha.run()
    .globals(['__flash_getWindowLocation', '__flash_getTopLocation'])
 
+   // Mocha calback - test end
+   .on('test end', function(data) {
+
+      var test = null,
+          obj = null,
+          title = '',
+          testName = '';
+
+      // Retrieve name of test
+      var obj = data;
+      while (obj.hasOwnProperty('parent')) {
+        title = obj.parent.title;
+        if (title) {
+          !testName ? testName = title : testName = title.concat(' >> ' + testName);
+        }
+        obj = obj.parent;
+      };
+
+      // Normalize test results
+      var test = {
+        name: testName,
+        status: data.state === 'passed' ? 'PASSED' : 'FAILED',
+        message: data.title,
+        stackTrace: data.hasOwnProperty('err') ? data.err.stack : ''
+      };
+      results.tests.push(test);
+   })
+
    // Mocha calback - HTML_JSON end
    .on('HTML_JSON end', function(data) {
 
-      // Retrieve test
-      $(data.suites).each(function(index, suite) {
-
-        // TO DO: Handle tests with multiple describes
-        if (suite.hasOwnProperty('test')) {
-          var obj = suite.test[0];
-          var test = {
-            name: suite.title,
-            status: obj.status === 'passed' ? 'PASSED' : 'FAILED',
-            message: obj.title ? obj.title : '',
-            stackTrace: obj.str ? obj.str : ''
-          };
-          results.tests.push(test);
-        };
-      });
-
-      // Retrieve final test results
+      // Normalize test summary
       results.done =  {
         passed: data.passes,
         failed: data.failures,
