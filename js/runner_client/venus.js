5dbcd409e8b1dfa39696a070c75df7e22ce67434
Doug Wallace <doug@dcwallace.com>
2012-10-03 11:29:25 -0700

    Extended the window.top console log to emit a socke.io event.

--- js/runner_client/venus.js
+++ js/runner_client/venus.js
@@ -1,20 +1,36 @@
 (function(window) {
   window.venus = new Venus();
-  
-  // var nativeLog = console.log;
+  var isMaster = (window === window.top) ? true : false
+  var nativeLog;
+  var socket = (isMaster) ? io.connect('http://'+window.location.hostname,{port:2013}) : null;
 
   function Venus() {}
-
- var socket = io.connect('http://'+window.location.hostname,{port:2013});
  
   Venus.prototype.done = function(results) {
     results.testId = window.venus.testId;
     socket.emit('done', results);
   };
 
-  // console.log = function(){
-  //   socket.emit('console.log', arguments);
-  //   nativeLog.apply(console,arguments);
-  // }
+  // this makes all consoles use the parent
+  // and makes the parent also emit via socket.io
+  nativeLog = (console && console.log) ? console.log : function() {};
+  if (isMaster) {
+    // top level window
+    if (console && console.log) {
+      console.log = function(){
+        nativeLog.apply(console,arguments);
+        var args = [].slice.call(arguments, 0);
+        socket.emit('console.log', args.join(","));
+      };
+    }
+  }
+  else {
+    // in an iframe, must be running a test
+    if (console && console.log) {
+      console.log = function() {
+        window.top.console.log.apply(this, arguments);
+      };
+    }
+  }
 
 }(window));
