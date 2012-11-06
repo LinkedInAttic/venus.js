2fb06bbcef16124dd0c5a1021f571bce22a39614
Seth McLaughlin <s@sethmcl.com>
2012-11-06 14:29:05 -0800

    Updating test suite to run all tests

--- test/specs/test_suite.js
+++ test/specs/test_suite.js
@@ -1,10 +1,14 @@
 // Defines the includes for all Venus tests to be run, which in turn
 // can be run using `npm test`
 
-var 
-    config = require('./config'),
-    executor = require('./executor'),
-    overlord = require('./overlord'),
-    testcase = require('./testcase'),
-testrun = require('./testrun'),
-    venus = require('./Venus');
+require('./config');
+require('./executor');
+require('./overlord');
+require('./testcase');
+require('./testrun');
+require('./Venus');
+
+require('./runners/phantom');
+
+require('./util/commentsParser');
+require('./util/pathHelper');
