87f0259209a2c05bdbe6b9bc0386650fe660fe3a
Seth McLaughlin <s@sethmcl.com>
2012-11-15 16:38:30 -0800

    Moved sample tests do examples folder. Added sample tests for Jasmine and QUnit.

---
+++ examples/01-SimpleMochaTest/test/Greeter.js
@@ -0,0 +1,15 @@
+/**
+ * @venus-library mocha
+ * @venus-include ../src/Greeter.js
+ */
+
+describe('Greeter', function() {
+
+  it('.talk() should format string', function() {
+    var greet  = new Greeter(),
+        result = greet.talk('Hello %s, how are you doing this fine %s?', 'Seth', 'Thursday');
+
+    expect(result).to.be('Hello Seth, how are you doing this fine Thursday?');
+  });
+
+});
