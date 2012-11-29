75b40bbcc7681c78a3b152956216255006076a91
Seth McLaughlin <s@sethmcl.com>
2012-11-29 14:24:46 -0800

    Implement socket.io connection between client and server.
    Supported events are:
    
    'results' -- test results
    'console.log' -- forward console.log messages from client for display in
    CLI

--- js/runner_client/venus.js
+++ js/runner_client/venus.js
@@ -1,32 +1,5 @@
-(function(window) {
-  window.venus = new Venus();
-
-  function Venus() {}
-
-  var XHR = (function() {
-    var ids = ["Msxml2.XMLHTTP", "Microsoft.XMLHTTP", "Msxml2.XMLHTTP.4.0"];
-    if (typeof XMLHttpRequest !== "undefined") {
-      return XMLHttpRequest;
-    }
-
-    for (var i = 0; i < ids.length; i++) {
-      try {
-        new ActiveXObject(ids[i]);
-        return function() {
-          return new ActiveXObject(ids[i]);
-        };
-        break;
-      } catch (e) {}
-    }
-  })();
-
-  Venus.prototype.done = function(results) {
-    var xmlhttp = new XHR();
-    var result = JSON.stringify(results);
-    xmlhttp.open("POST", window.venus.postUrl, true);
-    xmlhttp.setRequestHeader("Content-type","application/x-www-form-urlencoded");
-    xmlhttp.setRequestHeader("Content-length", result.length);
-    xmlhttp.setRequestHeader("Connection", "close");
-    xmlhttp.send(result);
-  };
-}(window));
+window.venus = new VenusClientLibrary({
+  host: 'http://' + window.location.hostname,
+  port: window.location.port
+});
+window.venus.connect();
--- test/specs/test_suite.js
+++ test/specs/test_suite.js
@@ -1,14 +1,14 @@
 // Defines the includes for all Venus tests to be run, which in turn
 // can be run using `npm test`
 
-require('./config');
-require('./executor');
-require('./overlord');
-require('./testcase');
-require('./testrun');
-require('./Venus');
+require('./config.spec');
+require('./executor.spec');
+require('./executor.socketio.spec');
+require('./testcase.spec');
+require('./testrun.spec');
+require('./Venus.spec');
 
-require('./uac/phantom');
+require('./uac/phantom.spec');
 
-require('./util/commentsParser');
-require('./util/pathHelper');
+require('./util/commentsParser.spec');
+require('./util/pathHelper.spec');
