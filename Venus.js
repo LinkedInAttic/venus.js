248dac0a5a09ef314e6341384e1922ad9af1f739
Seth McLaughlin <s@sethmcl.com>
2013-01-03 09:57:29 -0800

    Removing references to overlord, since this feature has been cut for version 1.

--- Venus.js
+++ Venus.js
@@ -6,7 +6,6 @@ var colors    = require('colors'),
     json5     = require('json5/lib/require'),
     _         = require('underscore'),
     Config    = require('./lib/config'),
-    overlord  = require('./lib/overlord'),
     executor  = require('./lib/executor'),
     i18n      = require('./lib/util/i18n'),
     locale    = require('./lib/util/locale'),
@@ -55,7 +54,6 @@ Venus.prototype.init = function (args) {
     .description( i18n('Run tests') )
     .option('-t, --test [tests]', i18n('Comma separated string of tests to run'))
     .option('-p, --port [port]', i18n('port to run on'), function(value) { return parseInt(value, 10); })
-    .option('-o, --overlord [url]', i18n('connect to an overlord server'))
     .option('-n, --phantom', i18n('Use phantomJS client to run browser tests'))
     .option('-s, --selenium', i18n('Use selenium client to run browser tests'))
     .option('-r, --selenium-server [url]', i18n('Specify selenium server to use'))
@@ -64,17 +62,6 @@ Venus.prototype.init = function (args) {
     .option('-v, --verbose', i18n('Run in verbose mode'))
     .option('-d, --debug', i18n('Run in debug mode'))
     .action( _.bind(this.startExecutor, this) );
-
-  // listen command  
-  // TODO: Re-enable once overlord code is implemented  
-  //program  
-    //.command('listen')  
-    //.description( i18n('Start the Overlord') )  
-    //.option('-p, --port [port]', i18n('port to run on'), function(value) { return parseInt(value, 10); })  
-    //.option('-l, --locale [locale]', i18n('Specify locale to use'))  
-    //.option('-v, --verbose', i18n('Run in verbose mode'))  
-    //.option('-d, --debug', i18n('Run in debug mode'))  
-    //.action( _.bind(this.startOverlord, this) );  
 
   program.parse(args);
 
@@ -96,14 +83,6 @@ Venus.prototype.applyCommandLineFlags = function(program) {
   }
 };
 
-// Start in overlord mode - server which allows browsers to be captured  
-Venus.prototype.startOverlord = function(program) {
-  this.applyCommandLineFlags(program);
-  logger.verbose( i18n('Starting Overlord') );
-  program.homeFolder = __dirname;
-  this.server = overlord.start(program);
-};
-
 // Start in Executor mode - run tests  
 Venus.prototype.startExecutor = function(program, defaultMode) {
   this.noCommand = false;
@@ -111,10 +90,6 @@ Venus.prototype.startExecutor = function(program, defaultMode) {
 
   this.applyCommandLineFlags(program);
   program.homeFolder = __dirname;
-
-  if(program.overlord === 1) {
-    program.overlord = overlord.defaultUrl;
-  }
 
   this.server = executor.start(program);
 };
--- test/specs/Venus.spec.js
+++ test/specs/Venus.spec.js
@@ -12,26 +12,6 @@ describe('Venus main', function() {
     (venus instanceof Venus).should.be.true;
   });
 
-  // TODO: enable test when functionality is implemented
-  //it('should call initialize project directory when init command is passed', function() {
-    //var argv = ['node', 'venus', 'init'],
-        //app  = new Venus();
-
-    //app.initProjectDirectory = sinon.spy();
-    //app.run(argv);
-    //app.initProjectDirectory.should.have.been.calledOnce;
-  //});
-
-  // TODO: Re-enable test when functionality is implemented
-  //it('should start overlord when listen command is passed', function() {
-    //var argv = ['node', 'venus', 'listen'],
-        //app  = new Venus();
-
-    //app.startOverlord = sinon.spy();
-    //app.run(argv);
-    //app.startOverlord.should.have.been.calledOnce;
-  //});
-
   it('should start executor when run command is passed', function() {
     var argv = ['node', 'venus', 'run'],
         app  = new Venus();
