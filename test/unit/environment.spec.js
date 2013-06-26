cd853f1ac8af4dabc085f1804b2b808eea0c528c
Seth McLaughlin <s@sethmcl.com>
2013-06-26 02:14:29 -0700

    Version 2.0 beta
    * Refactored PhantomJS support to utilize GhostDriver, instead of
      PhantomJSPlease
    * Refactored UAC design to be more stable and avoid wasting resources
    * By default, Venus will now ignore all test files that do not contain
      at least one @venus-* annotation
    * Switched from webdriverjs to selenium-webdriver NPM module for
      interfacing with webdriver API

--- test/unit/environment.spec.js
+++ test/unit/environment.spec.js
@@ -2,13 +2,13 @@
  * @author LinkedIn
  */
 'use strict';
-var environment   = require('../../lib/environment'),
-    sinon         = require('sinon'),
-    path          = require('flavored-path'),
-    expect        = require('expect.js'),
-    phantomUac    = require('../../lib/uac/phantom'),
-    _s            = require('underscore.string'),
-    helpers       = require('../lib/helpers.js');
+var environment    = require('../../lib/environment'),
+    sinon          = require('sinon'),
+    path           = require('flavored-path'),
+    expect         = require('expect.js'),
+    GhostDriverUac = require('../../lib/uac/GhostDriverUac'),
+    _s             = require('underscore.string'),
+    helpers        = require('../lib/helpers.js');
 
 describe('lib/environment', function () {
   var env;
@@ -21,11 +21,11 @@ describe('lib/environment', function () {
     var envConfig;
 
     beforeEach(function () {
-      envConfig = env.loadConfig('phantomjs');
+      envConfig = env.loadConfig('ghost');
     });
 
     it('should load the UAC properly', function () {
-      expect(env.uac).to.be(phantomUac);
+      expect(env.Uac).to.be(GhostDriverUac);
     });
 
     it('should provide absolute paths', function () {
---
+++ test/unit/uac/GhostDriverUac.spec.js
@@ -0,0 +1,9 @@
+/**
+ * @author LinkedIn
+ */
+var expect         = require('expect.js'),
+    GhostDriverUac = require('../../../lib/uac/GhostDriverUac'),
+    http           = require('http');
+
+describe('GhostDriverUac', function () {
+});
