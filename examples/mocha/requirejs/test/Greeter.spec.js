d17020ac80ab20b3e301e8ba7b827ea0ef47e0c1
Seth McLaughlin <s@sethmcl.com>
2013-07-14 16:53:38 -0700

    RequireJS & InjectJS example tests
    * Updated examples for testing requirejs modules
    * Added example for testing injectjs modules

---
+++ examples/mocha/requirejs/test/Greeter.spec.js
@@ -0,0 +1,21 @@
+/**
+ * @venus-library mocha
+ * @venus-include ../lib/require.js
+ * @venus-include ../Greeter.js
+ */
+
+describe('Greeter', function() {
+
+  it('.bye() should say goodbye', function(done) {
+
+    require(['Greeter'], function (Greeter) {
+      var greet  = new Greeter(),
+          result = greet.bye(true);
+
+      expect(result).to.be('goodbye...');
+      done();
+    });
+
+  });
+
+});
