c67bc23d282713cdb198b8189b6d65236c98b37b
Seth McLaughlin <s@sethmcl.com>
2013-08-28 09:50:20 -0700

    Fix for relative path includes
    Fixes issue #242:
    
    ```js
    /**
     * @venus-include ./test.js
     */
    ```
    
    Now behaves the same as:
    
    ```js
    /**
     * @venus-include test.js
     */
    ```

--- examples/mocha/Greeter/specs/Greeter.spec.js
+++ examples/mocha/Greeter/specs/Greeter.spec.js
@@ -1,11 +1,11 @@
 /**
  * @venus-library mocha
- * @venus-code ../src/Greeter.js
+ * @venus-code ./Greeter.js
  */
 
 describe('Greeter', function() {
 
-  it('.talk() should <b>format</b> string', function() {
+  it('.talk() should <b>format string</b>', function() {
     var greet  = new Greeter(),
         result = greet.talk('Hello %s, how are you doing this fine %s?', 'Seth', 'Thursday');
 
