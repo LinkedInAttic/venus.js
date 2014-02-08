64706264de653c2febc2d29993349857dc398a9e
Roel Ramirez <rramirez@rramirez-ld.linkedin.biz>
2014-02-07 18:35:58 -0800

    updated docs for qunit-venus.js

--- .venus/adaptors/venus-mocha-1.12.0.js
+++ .venus/adaptors/venus-mocha-1.12.0.js
@@ -1,8 +1,27 @@
-// @author LinkedIn
+/**
+ * Venus
+ * Copyright 2013 LinkedIn
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *     Unless required by applicable law or agreed to in writing,
+ *     software distributed under the License is distributed on an "AS
+ *     IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ *     express or implied.   See the License for the specific language
+ *     governing permissions and limitations under the License.
+ */
 
-// Create a Mocha adaptor which inherits methods from the adapter template (.venus/adapters/adaptor-template.js)
+/**
+ * Create a Mocha adaptor which inherits methods from the adapter template (.venus/adapters/adaptor-template.js)
+ */
 
-// Setup Mocha with behavior driven development (BDD)
+/**
+ * Setup Mocha with behavior driven development (BDD)
+ */
 function Adaptor() {
   mocha.setup({ ui: 'bdd', ignoreLeaks: true });
 };
@@ -10,11 +29,12 @@ function Adaptor() {
 // Inherit from adapter template
 Adaptor.prototype = new AdaptorTemplate();
 
-// Override Methods
-// ----------------
-
-// Override methods defined in the adapter template
-
+/**
+ * ### start
+ * Start and process test results
+ * @method start
+ * @return {Void}
+ */
 Adaptor.prototype.start = function() {
   var self, fixtureHelper, mochaDiv, runner, tests, complete;
 
@@ -59,10 +79,24 @@ Adaptor.prototype.start = function() {
   runner.run();
 };
 
+/**
+ * ### getTestMessage
+ * Get message from test result
+ * @method getTestMessage
+ * @param {Object} data test result data
+ * @return {String} test result message
+ */
 Adaptor.prototype.getTestMessage = function(data) {
   return data.title ? data.title : '';
 };
 
+/**
+ * ### getTestName
+ * Get name from test result
+ * @method getTestName
+ * @param {Object} data test result data
+ * @return {String} test result name
+ */
 Adaptor.prototype.getTestName = function(data) {
   var obj = null,
       title = '',
@@ -81,26 +115,68 @@ Adaptor.prototype.getTestName = function(data) {
   return testName;
 };
 
+/**
+ * ### getTestStatus
+ * Get status from test result
+ * @method getTestStatus
+ * @param {Object} data test result data
+ * @return {Boolean} test result status
+ */
 AdaptorTemplate.prototype.getTestStatus = function(data) {
   return data.state === 'passed' ? this.ENUM_STATE.PASSED : this.ENUM_STATE.FAILED;
 };
 
+/**
+ * ### getTestStackTrace
+ * Get stack trace from test result
+ * @method getTestStackTrace
+ * @param {Object} data test result data
+ * @return {String} test result stack trace
+ */
 AdaptorTemplate.prototype.getTestStackTrace = function(data) {
   return data.hasOwnProperty('err') ? data.err.stack : '';
 };
 
+/**
+ * ### getTotal
+ * Get total number of tests
+ * @method getTotal
+ * @param {Object} data test result data
+ * @return {Integer} total number of tests
+ */
 AdaptorTemplate.prototype.getTotal = function(data) {
   return data.passes + data.failures;
 };
 
+/**
+ * ### getTotalFailed
+ * Get total number of failed tests
+ * @method getTotalFailed
+ * @param {Object} data test result data
+ * @return {Integer} total number of failed tests
+ */
 AdaptorTemplate.prototype.getTotalFailed = function(data) {
   return data.failures;
 };
 
+/**
+ * ### getTotalPassed
+ * Get total number of passed tests
+ * @method getTotalPassed
+ * @param {Object} data test result data
+ * @return {Integer} total number of passed tests
+ */
 AdaptorTemplate.prototype.getTotalPassed = function(data) {
   return data.passes;
 };
 
+/**
+ * ### getTotalRuntime
+ * Get total runtime of running all tests
+ * @method getTotalRuntime
+ * @param {Object} data test result data
+ * @return {Integer} total runtime
+ */
 AdaptorTemplate.prototype.getTotalRuntime = function(data) {
   return data.milliseconds;
 };
