96dea8523763b9fec7cbb56e5ad1636baf697fa0
Geoff Berger <gberger@linkedin.com>
2015-03-06 14:37:39 -0800

    created skeleton for runner_client tests

---
+++ test/unit/client/runner_client/VenusUi.spec.js
@@ -0,0 +1,8 @@
+/**
+ * @venus-code runnerClient/VenusUi.js
+ */
+describe('VenusUi', function() {
+  it('should expect on the window as VenusUi', function() {
+    expect(window.VenusUi).to.be.ok();
+  });
+});
